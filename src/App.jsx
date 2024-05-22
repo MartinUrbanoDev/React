c413fff620eb979b30f3a0cc53588e849a35b920
Martin <martinurb00@gmail.com>
2024-05-22 18:25:45 -0300

    new style buttons

--- src/App.jsx
+++ src/App.jsx
@@ -40,18 +40,18 @@ function App() {
 
   return (
     <div className="min-h-screen bg-[#2D2D2D] flex items-center justify-center">
-      <div className="bg-[#f7efef] p-6 rounded shadow-md w-full max-w-md">
+      <div className="bg-[#f7efef] p-6 rounded shadow-md w-full max-w-md border-8 border-b border-r border-l border-green-500">
         <h1 className="text-2xl font-bold mb-4">Todo List</h1>
         <div className="flex mb-4">
           <input
             type="text"
-            className="focus:shadow-lg focus:shadow-blue-400 pl-12 w-full py-4 bg-gray-700 rounded-xl outline-none transition-all duration-300 ease-in-out"
+            className="focus:shadow-md focus:shadow-blue-400 pl-12 w-full py-4 bg-blue-200 rounded-xl outline-none transition-all duration-300 ease-in-out"
             value={newTask}
             onChange={(e) => setNewTask(e.target.value)}
             onKeyDown={handleKeyDown}
           />
           <button
-            className="bg-blue-500 text-white p-2 ml-2"
+            className="bg-blue-500 text-white p-2 ml-2 rounded-lg transition transform active:scale-95"
             onClick={editingTask ? handleEditTask : handleAddTask}
           >
             {editingTask ? 'Edit Task' : 'Add Task'}
@@ -63,13 +63,13 @@ function App() {
               <span>{task.text}</span>
               <div>
                 <button
-                  className="bg-orange-500 text-white p-2 mr-2"
+                  className="bg-orange-500 text-white p-2 mr-2 rounded-full transition transform active:scale-90"
                   onClick={() => handleStartEditingTask(task)}
                 >
                   <EditInput />
                 </button>
                 <button
-                  className="bg-red-500 text-white p-2"
+                  className="bg-red-500 text-white p-2 rounded-full transition transform active:scale-90"
                   onClick={() => handleDeleteTask(task.id)}
                 >
                   <Trash />
